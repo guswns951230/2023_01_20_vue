b7069761d4ff47af082c8a000aa1e8fb2dd67b0a
Kang <68116861+guswns951230@users.noreply.github.com>
2023-01-20 15:36:20 +0900

    20230120 vue project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 5,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 class="count">{{ count }}</h1>
+  <p>vue로 확인하는 웹페이지입니다. 아래버튼을 누르면 값이 1씩 증가합니다.</p>
+  <button class="btn" v-on:click="increment">+1</button>
+</template>
+
+<style>
+.count {
+  color: red;
+}
+.btn {
+  border: 2px solid black;
+  border-radius: 3px;
+}
+</style>
